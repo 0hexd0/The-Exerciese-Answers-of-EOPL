ba8caf0d207c2b1af6994a50a67bbd468df873e3
ADong <1514133606@qq.com>
2020-03-20 10:44:55 +0800

    Create 2.4.rkt

---
+++ 2.4.rkt
@@ -0,0 +1,18 @@
+#lang racket
+
+;(define e
+;  (extend-env ’d 6
+;              (extend-env ’y 8
+;                          (extend-env ’x 7
+;                                      (extend-env ’y 14
+;                                                  (empty-env))))))
+
+;(define stack
+;  (pop
+;   (push empty-stack 1)))
+
+;constructors: empty-stack
+
+;observers: push pop top empty-stack?
+
+;CPS?
