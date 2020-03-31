af07dc959fc9c279aa066d033a8920ba67be9172
ADong <1514133606@qq.com>
2020-03-31 18:52:10 +0800

    Update 3.2.rkt

--- chapter3/section1/3.2.rkt
+++ chapter3/section1/3.2.rkt
@@ -2,5 +2,5 @@
 ;⌈⌊val⌋⌉ != val
 ;set val = (bool-val true)
 ;so (expval->num val) = undefined
-;so (num-val (expval->num val)) = (num-val undefined) = undefined != (bool-val true)
+;so (num-val (expval->num val)) = (num-val undefined) = undefined? != (bool-val true)
 ;so (num-val (expval->num val)) != val
